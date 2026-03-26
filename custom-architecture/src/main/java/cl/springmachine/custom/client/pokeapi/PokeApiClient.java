f74fd634cbd74bfb12859b09e4e318d791191c9c
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2026-03-26 02:54:25 -0300

    delete: Drop custom architecture

--- custom-architecture/src/main/java/cl/springmachine/custom/client/pokeapi/PokeApiClient.java
+++
@@ -1,8 +0,0 @@
-package cl.springmachine.custom.client.pokeapi;
-
-import cl.springmachine.custom.client.pokeapi.dto.PokemonPokeApiDTO;
-
-public interface PokeApiClient {
-
-    PokemonPokeApiDTO getPokemonInfo(String name);
-}
